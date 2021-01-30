c466493ef16f0902e68380b746ceeffa067e4e89
Benjamin Pang <benjinpang@gmail.com>
2021-01-30 13:08:54 +0400

    style page

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@ const markov = new Markov(chain)
 const App = {
   data(){
     return {
+      mounted: false,
       text: ''
     }
   },
   mounted(){
+    this.mounted = true
     let iterate = setInterval(_=>{
       let next = markov.next()
       if(!next) return clearInterval(iterate)
-      let last = this.text[this.text.length - 1]
-      if(!next.match(/['.?!,@:)]/) && (last && !last.match(/[`(]/))){
+      let last = markov.history[markov.history.length - 2]
+      if(!next.match(/^([.?!,@:;")]|'s|n't|'re)$/) && (last && !last.match(/[`(@$]/))){
         this.text += ' '
       }
       this.text += next
